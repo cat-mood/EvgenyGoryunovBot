8b3dd70a5ede37dc1c7dbbc407dc4609129acdee
Timofey Golubev <2timmoff3@gmail.com>
2023-08-03 23:20:26 +0300

    Move start func into handlers

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import asyncio
 import logging
 import os
+import db
+from handlers import funcs
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token=os.getenv("TOKEN"))
-dp = Dispatcher()
-
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Test message")
 
 async def main():
+    bot = Bot(token=os.getenv("TOKEN"))
+    dp = Dispatcher()
+
+    dp.include_routers(funcs.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
